14a46793cb45e8c15fc8100b9677c6f0f006047e
Christian Hamamdjian <christian.hamamdjian@welho.com>
2019-06-11 11:43:36 +0300

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "../node_modules/react";
 import "./App.css";
 import AddEntryComponent from "./components/AddEntryComponent";
 
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      transList:[],
+      transList: [],
       balance: 0
     };
     this.passToList = this.passToList.bind(this);
@@ -15,31 +15,60 @@ class App extends Component {
     console.log(myTrans);
     this.setState({
       transList: [...this.state.transList, myTrans]
-    });  
+    });
   }
   render() {
+    let myBalance = 0;
+    const myIncome = this.state.transList
+      .filter(element => element.type === "income")
+      .map((element, i) => {
+        myBalance += Number(element.amount);
+        return (
+          <div key={element.description + "-" + i}>
+            <span>{element.description}</span>
+            <span>{element.amount}</span>
+            <span>
+              {" "}
+              {element.date}
+              {element.time}
+            </span>
+          </div>
+        );
+      });
 
-    let myBalance = 0
-    const myIncome = this.state.transList.filter(element => element.type === 'income').map((element,i) => {
-      myBalance += Number(element.amount);
-       return (<div key = {element.description +"-" + i} ><span>{element.description}</span><span>{element.amount}</span><span> {element.date}{element.time}</span></div>)
-    });
-
-    const myExpense = this.state.transList.filter(element => element.type === 'expense').map((element,i) => {
-      myBalance -= Number(element.amount);
-      return (<div key = {element.description +"-"+ i} ><span>{element.description}</span><span>{element.amount}</span><span> {element.date}{element.time}</span></div>)
-    });
-    console.log(this.state)
+    const myExpense = this.state.transList
+      .filter(element => element.type === "expense")
+      .map((element, i) => {
+        myBalance -= Number(element.amount);
+        return (
+          <div key={element.description + "-" + i}>
+            <span>{element.description}</span>
+            <span>{element.amount}</span>
+            <span>
+              {" "}
+              {element.date}
+              {element.time}
+            </span>
+          </div>
+        );
+      });
+    console.log(this.state);
     return (
       <div className="App">
-        <h1>Account Balance Tracker</h1>
+        <div className="title">Account Balance Tracker</div>
         <AddEntryComponent passToList={this.passToList} />
         <div id="lists">
-        <div id ="income"><h2>Income:</h2>{myIncome}</div>
-        <div id ="expense"><h2>Expenses:</h2>{myExpense}</div>
+          <div id="income">
+            <h2>Income:</h2>
+            {myIncome}
+          </div>
+          <div id="expense">
+            <h2>Expenses:</h2>
+            {myExpense}
+          </div>
         </div>
-        <h3>Balance</h3>
-        <h3>{myBalance}</h3>
+        <div className="balance">Balance </div>
+        <div className="balance-number">{myBalance}</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "../node_modules/react";
+import ReactDOM from "../node_modules/react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
